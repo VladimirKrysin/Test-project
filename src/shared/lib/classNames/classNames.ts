5d705b91e6bc66b4f2b99dc516980ac39b24362c
Vladimir <vovakrysinn@gmail.com>
2025-06-23 10:18:35 +0300

    added jest with first test of classNames function

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,17 +1,13 @@
 type Mods = Record<string, boolean | string>;
 
-function classNames(
-  cls: string,
-  mods: Mods = {},
-  additional: string[] = []
-): string {
+function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
   return [
     cls,
     ...additional.filter(Boolean),
     ...Object.entries(mods)
       .filter(([cls, value]) => Boolean(value))
       .map(([cls, value]) => cls),
-  ].join(" ");
+  ].join(' ');
 }
 
 export { classNames };
