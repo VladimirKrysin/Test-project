535479130affb1cdfc24913eaebad4d4cd810053
Vladimir <vovakrysinn@gmail.com>
2025-06-25 09:14:44 +0300

    fixed linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintReact, { rules } from 'eslint-plugin-react';
@@ -10,46 +7,56 @@ import prettierPlugin from 'eslint-plugin-prettier';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import i18next from 'eslint-plugin-i18next';
 
-export default tseslint.config({
-  plugins: {
-    '@typescript-eslint': tseslint.plugin,
-    react: eslintReact,
-    'react-hooks': eslintReactHooks,
-    'react-refresh': eslintReactRefresh,
-    prettier: prettierPlugin,
-  },
-}, {
-  ignores: ['dist', 'node_modules', 'eslint.config.js'],
-}, js.configs.recommended, ...tseslint.configs.recommended, {
-  languageOptions: {
-    parserOptions: {
-      project: ['tsconfig.json'],
+export default tseslint.config(
+  {
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      react: eslintReact,
+      'react-hooks': eslintReactHooks,
+      'react-refresh': eslintReactRefresh,
+      prettier: prettierPlugin,
     },
   },
-}, i18next.configs['flat/recommended'], {
-  files: ['**/*.{ts,tsx}'],
-  rules: {
-    ...prettierPlugin.configs.recommended.rules,
-    ...eslintConfigPrettier.rules,
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    'prefer-const': 'error',
-    'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
-    'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
-    'react/self-closing-comp': ['error', { component: true, html: true }],
-    'i18next/no-literal-string': [
-      'warn',
-      { markupOnly: true, ignoreAtttribute: ['data-testid', 'to'] },
-    ],
-    'max-lines': ['warn', { max: 124 }],
-    'max-params': ['error', 3],
+  {
+    ignores: ['dist', 'node_modules', 'eslint.config.js'],
   },
-}, {
-  overrides: [
-    {
-      files: ['**/src/**/*.test.{ts,tsx}'],
-      rules: {
-        'i18next/no-literal-string': 'off',
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: ['tsconfig.json'],
       },
     },
-  ],
-}, storybook.configs["flat/recommended"]);
+  },
+  i18next.configs['flat/recommended'],
+  {
+    files: ['**/*.{ts,tsx}'],
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prefer-const': 'error',
+      'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
+      'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
+      'react/self-closing-comp': ['error', { component: true, html: true }],
+      'i18next/no-literal-string': [
+        'warn',
+        { markupOnly: true, ignoreAtttribute: ['data-testid', 'to'] },
+      ],
+      'max-lines': ['warn', { max: 124 }],
+      'max-params': ['error', 3],
+    },
+  },
+  {
+    overrides: [
+      {
+        files: ['**/src/**/*.test.{ts,tsx}'],
+        rules: {
+          'i18next/no-literal-string': 'off',
+        },
+      },
+    ],
+  },
+  storybook.configs['flat/recommended'],
+);
